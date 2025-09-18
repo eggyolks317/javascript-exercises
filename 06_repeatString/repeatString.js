06b690d7a958aec3aff155fe14c77756f0bf93cf
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-18 14:46:00 -0700

    finished

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let ans = str;
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else if (num == 0) {
+        return '';
+    }
+    for (let i = 0; i< num-1; i ++) {
+        ans += str;
+    }
+    return ans;
 };
 
 // Do not edit below this line
