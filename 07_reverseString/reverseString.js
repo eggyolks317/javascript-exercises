d2c5f1cc6867bd6b6073fe5a93d9951b58195a58
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-18 14:52:29 -0700

    finished

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let ans = '';
+    for (let i = str.length; i >= 0; i--) {
+        ans += str.charAt(i);
+    }
+    return ans;
 };
 
 // Do not edit below this line
