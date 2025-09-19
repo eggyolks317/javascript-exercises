f4f9d7416b88c2188dc401fda95f0efb11771b1e
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-19 00:00:54 -0700

    finished used Number.isInteger to check if it is an interger used .toFixed() to round the numbers used parseFloat() to change the rounded numbers from string to float

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let ans = ((num) - 32) * (5/9);
+  if (Number.isInteger(ans)) {
+    return ans;
+  }
+  return parseFloat(ans.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let ans = (num) * (9/5) + 32;
+  if (Number.isInteger(ans)) {
+    return ans;
+  }
+  return parseFloat(ans.toFixed(1));
 };
 
 // Do not edit below this line
