eeaf2747fe546d939e614652a5caacf120123dde
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-18 23:48:07 -0700

    finished learned how to use Number.isNumber to check if it is integer

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if ((a<0) || (b<0)) {
+        return 'ERROR';
+    }
+    else if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a<b) {
+            while (a <= b) {
+                sum += a;
+                a++;
+            }
+        }
+        else {
+            while (a >= b) {
+                sum += a;
+                a--;
+            }
+        }
+        return sum;
+    }
+    else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
