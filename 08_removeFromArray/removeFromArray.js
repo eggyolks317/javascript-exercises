a5ba7387bb470ea3a5b9993cdaeab4ccd9c6e846
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-18 23:36:50 -0700

    finished
    learned to use filter to remove items from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...item) {
+    return arr.filter((value) => !item.includes(value));
 };
 
 // Do not edit below this line
