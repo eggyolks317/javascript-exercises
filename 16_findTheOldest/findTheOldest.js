ec07a91cd6ae3b99b3d9cbc29072d35841008a76
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-30 22:24:56 -0700

    used Array.reduce() to help finish exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, curr) => {
+    let currentYear = new Date().getFullYear();
+    let currentAge = (curr.yearOfDeath || currentYear) - curr.yearOfBirth;
+    let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    if (currentAge > oldestAge) {
+      return curr;
+    } else {
+      return oldest;
+    }
+  });
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
