84c10478a1fc0df52ee045fab908d4bdd1d66022
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-30 21:57:22 -0700

    finished

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n == 0) {
+    return 0;
+  } else if (n < 0) {
+    return "OOPS";
+  }
+  let one = 1;
+  let two = 1;
+  n -= 2;
+  for (; n > 0; n--) {
+    let temp = one;
+    one = two;
+    two = temp + two;
+  }
+  return two;
 };
 
 // Do not edit below this line
