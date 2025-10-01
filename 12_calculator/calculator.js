c2c529808b7a3a4ee59bab54bbbe548d55f0ac1a
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-30 20:35:53 -0700

    finished exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, value) => (sum += value), 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((product, value) => (product *= value));
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  let ans = 1;
+  for (; exp > 0; exp--) {
+    ans *= base;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let ans = 1;
+  for (; n > 0; n--) {
+    ans *= n;
+  }
+  return ans;
 };
 
 // Do not edit below this line
@@ -29,5 +37,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
