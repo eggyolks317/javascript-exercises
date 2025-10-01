299c6d0c50160ffb675ea4bd73aa52b08a2fcf37
eggyolks317 <yokshi25@kas.kh.edu.tw>
2025-09-30 21:48:52 -0700

    finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = reform(str);
+  let front, back;
+  let count = 0;
+  if (str.length % 2 == 0) {
+    front = str.slice(0, str.length / 2).split("");
+    back = str.slice(str.length / 2, str.length).split("");
+  } else {
+    front = str.slice(0, str.length / 2).split("");
+    back = str.slice(str.length / 2 + 1, str.length).split("");
+  }
+  for (let i = 0; i < front.length; i++) {
+    if (front[i] == back[front.length - 1 - i]) {
+      count++;
+    }
+  }
+  return count == front.length;
 };
+
+function reform(str) {
+  return str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
